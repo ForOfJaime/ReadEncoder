768a56a5289a5faad13945a76f20482435ae28d7
ForOfJaime <jgcbateria01@gmail.com>
2022-12-05 17:32:37 +0100

    comienza la creacion del menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,14 @@
 #include <Arduino.h>
+#include <U8g2lib.h>
+
+#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+#endif
+#ifdef U8X8_HAVE_HW_I2C
+#include <Wire.h>
+#endif
+
+U8G2_ST7920_128X64_F_SW_SPI u8g2(U8G2_R0, /* clock=*/ 13, /* data=*/ 11, /* CS=*/ 10, /* reset=*/ 8);
 
 //Lista de pines entrada encoder//
 const int channelPinA = 3;
@@ -16,6 +26,7 @@ bool IsCW = true;
 //Velocidad cominucacion serial
 int serialSpeed = 9600;
 
+String screens[] = {"Boost", "Oil Temp", "Oil Press", "Water temp", "Options"};
 
 
 void doEncode()
@@ -39,19 +50,35 @@ void doEncode()
 
 void setup() {
   Serial.begin(serialSpeed);
+  u8g2.begin();
   pinMode(channelPinA, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(channelPinA), doEncode, CHANGE);
+
+  u8g2.clearBuffer();
+  u8g2.setFont(u8g2_font_ncenB08_tr);
+  for (int i = 1; i < 6; i++)
+  {
+    u8g2.setCursor(10,i*10);
+    u8g2.print(screens[i - 1]);
+  }
+  u8g2.sendBuffer();
 }
 
 
 void loop() {
 
- if (counter != ISRCounter)
-   {
-      counter = ISRCounter;
-      Serial.println(counter);
-   }
-  
 
+
+
+  if (counter != ISRCounter){
+      counter = ISRCounter;
+      String str = String(counter/2);
+      Serial.println(str);
+      u8g2.clearBuffer();
+      u8g2.setFont(u8g2_font_ncenB08_tr);
+      u8g2.setCursor(20,20);
+      u8g2.print(str);
+      u8g2.sendBuffer();
+    }
 }
 
