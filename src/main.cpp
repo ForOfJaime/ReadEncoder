fc15de81af5d8f02fa6e1265bf9861d20b737e7f
ForOfJaime <jgcbateria01@gmail.com>
2022-12-07 00:31:01 +0100

    creando menú principal

--- src/main.cpp
+++ src/main.cpp
@@ -14,17 +14,21 @@ U8G2_ST7920_128X64_F_SW_SPI u8g2(U8G2_R0, /* clock=*/ 13, /* data=*/ 11, /* CS=*
 const int channelPinA = 3;
 const int channelPinB = 2;
 const int timeThreshold = 5;
+const int pulsador = 6;
 
 long timeCounter = 0;
 
 
-volatile int ISRCounter = 0;
-int counter = 0;
+volatile int ISRCounter = 1;
+int counter = 1;
 
 bool IsCW = true;
 
 //Velocidad cominucacion serial
 int serialSpeed = 9600;
+
+bool menuInicialited = false;
+bool menuFirstStart = false;
 
 String screens[] = {"Boost", "Oil Temp", "Oil Press", "Water temp", "Options"};
 
@@ -47,38 +51,63 @@ void doEncode()
    }
 }
 
+void printMainMenu(){
+  
+  while(menuInicialited == true){
+    u8g2.setFont(u8g2_font_ncenB08_tr);
+    u8g2.clearBuffer();
+    for (int i = 1; i < 6; i++)
+    {
+      u8g2.setCursor(13,(i*12));
+      u8g2.print(screens[i - 1]);
+    }
+    u8g2.setFont(u8g2_font_unifont_t_symbols);
+    u8g2.drawGlyph(0,((counter / 2) *12 + 2), 0x23F5);	
+    Serial.println(counter / 2);
+    u8g2.sendBuffer(); 
+    if (counter != ISRCounter){
+    counter  = ISRCounter;
+    }
+    u8g2.setFont(u8g2_font_ncenB08_tr);
+    u8g2.clearBuffer();
+    if (menuFirstStart == false){
+      menuFirstStart = true;
+      u8g2.setFont(u8g2_font_ncenB08_tr);
+      u8g2.clearBuffer();
+      for (int i = 1; i < 6; i++)
+      {
+      u8g2.setCursor(13,(i*12));
+      u8g2.print(screens[i - 1]);
+      }
+      u8g2.setFont(u8g2_font_unifont_t_symbols);
+      u8g2.drawGlyph(0,((counter / 2) *12 + 2), 0x23F5);	
+      Serial.println(counter / 2);
+      u8g2.sendBuffer(); 
+      if (counter != ISRCounter){
+      counter  = ISRCounter;
+      }
+      u8g2.setFont(u8g2_font_ncenB08_tr);
+      u8g2.clearBuffer();
+    }
+    
+  }
+  
+}
 
 void setup() {
   Serial.begin(serialSpeed);
   u8g2.begin();
   pinMode(channelPinA, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(channelPinA), doEncode, CHANGE);
-
-  u8g2.clearBuffer();
-  u8g2.setFont(u8g2_font_ncenB08_tr);
-  for (int i = 1; i < 6; i++)
-  {
-    u8g2.setCursor(10,i*10);
-    u8g2.print(screens[i - 1]);
-  }
-  u8g2.sendBuffer();
 }
 
 
 void loop() {
+ printMainMenu();
+  if(!digitalRead(pulsador)){
+    menuInicialited = true;
+  }
 
-
-
-
-  if (counter != ISRCounter){
-      counter = ISRCounter;
-      String str = String(counter/2);
-      Serial.println(str);
-      u8g2.clearBuffer();
-      u8g2.setFont(u8g2_font_ncenB08_tr);
-      u8g2.setCursor(20,20);
-      u8g2.print(str);
-      u8g2.sendBuffer();
-    }
+  
 }
 
